7046a42fcd1444ca08591272bb2336d50fc9eae2
mirzaali45 <mirzaaliyusuf45@gmail.com>
2024-10-22 21:58:35 +0700

    Modul 2-2

---
+++ exercise/kalkulator.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display')
+const clear = document.getElementById('clear')
+const equal = document.getElementById('equal')
+
+document.querySelectorAll('.btn').forEach((button) => {
+    button.addEventListener('click', () => {
+        display.value += button.textContent
+    })
+})
+clear.addEventListener('click', () => {
+    display.value = ''
+})
+
+equal.addEventListener('click', () => {
+    display.value = eval(display.value)
+})
+document.addEventListener('keydown', (e) => {
+    const value = '1234567890+-/*.'.split('')
+    const key = e.key
+    if (value.includes(key)){
+        display.value += key
+    }
+    if (key == 'Enter') {
+        display.value = eval(display.value)
+    }
+})
